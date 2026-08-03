a2f57c5299c8cb9a094afea01bffe5dfd53f7831
dosssman <dosssman@hotmail.fr>
2026-08-03 14:42:00 +0900

    WIP on porting the previous dosssman keymaping and fixing it so it can build

---
+++ keyboards/bastardkb/charybdis/4x6/keymaps/dosssman/config.c
@@ -0,0 +1,102 @@
+#include QMK_KEYBOARD_H
+
+
+typedef union {
+  uint32_t raw;
+  struct {
+    bool     rgb_layer_change :1;
+  };
+} user_config_t;
+
+user_config_t user_config;
+
+#define LAYER_0 0 // Base layer 0 for general usage
+#define LAYER_1 1 // Base layer 1 for general usage
+#define LAYER_2 2 // Base layer 2 for general usage
+
+/* RTS Layers:
+  - Sacrifices desktop navigations to have more access to control groups
+  - more convenient Shift and Control positions for control group creations
+  - replaces mouse keys on the left side with corresponding keys from the right (as if mirroring) for more shortcuts access
+*/
+#define LAYER_3 3 // PAT || RTS focused base layer 0
+#define LAYER_4 4 // PAT || RTS focused layer 1
+#define LAYER_5 5 // PAT || RTS focused layer 2
+
+/* FPS layers:
+  - replaces mouse keys on the left side with corresponding keys from the right (as if mirroring) for more shortcuts access
+*/
+#define LAYER_6 6 // FPS Base Layer 0
+#define LAYER_7 7 // FPS Focused Layer 1
+#define LAYER_8 8 // FPS Focused Layer 2
+
+#define LAYERS_EFFECT_SPEEDS ((uint8_t[]){ 64, 148, 255})
+
+void keyboard_post_init_user(void) {
+  // Call the keymap level matrix init.
+
+  // Read the user config from EEPROM
+  user_config.raw = eeconfig_read_user();
+
+  // Set default layer, if enabled
+  if (user_config.rgb_layer_change) {
+    rgblight_enable();
+    rgblight_sethsv_noeeprom(HSV_CYAN);
+    rgblight_mode_noeeprom(RGB_MATRIX_BREATHING);
+    rgb_matrix_set_speed_noeeprom(LAYERS_EFFECT_SPEEDS[0]);
+  }
+}
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+    const uint32_t curr_layer = get_highest_layer(state);
+
+    switch (curr_layer) {
+    case LAYER_0 ... LAYER_2:
+      if (user_config.rgb_layer_change) {
+        rgblight_sethsv_noeeprom(HSV_CYAN);
+        // rgb_matrix_set_speed_noeeprom(LAYERS_EFFECT_SPEEDS[curr_layer % 3]);
+      };
+      break;
+    case LAYER_3 ... LAYER_5:
+      if (user_config.rgb_layer_change) {
+        rgblight_sethsv_noeeprom(HSV_SPRINGGREEN);
+        // rgb_matrix_set_speed_noeeprom(LAYERS_EFFECT_SPEEDS[curr_layer % 3]);
+      }
+      break;
+    default: //  for any other layers, or the default layer
+      if (user_config.rgb_layer_change) {
+        rgblight_sethsv_noeeprom(HSV_CYAN);
+        rgblight_mode_noeeprom(RGB_MATRIX_BREATHING);
+        // rgb_matrix_set_speed_noeeprom(LAYERS_EFFECT_SPEEDS[0]); // Maintain same effect speed across layer groups
+      }
+      break;
+    }
+  return state;
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case RGB_MODE_FORWARD ... RGB_MODE_GRADIENT: // For any of the RGB codes (see quantum_keycodes.h, L400 for reference)
+        if (record->event.pressed) { //This disables layer indication, as it's assumed that if you're changing this ... you want that disabled
+            if (user_config.rgb_layer_change) {        // only if this is enabled
+                user_config.rgb_layer_change = false;  // disable it, and
+                eeconfig_update_user(user_config.raw); // write the setings to EEPROM
+            }
+        }
+        return true; break;
+    default:
+      return true; // Process all other keycodes normally
+  }
+}
+
+void eeconfig_init_user(void) {  // EEPROM is getting reset!
+  user_config.raw = 0;
+  user_config.rgb_layer_change = true; // We want this enabled by default
+  eeconfig_update_user(user_config.raw); // Write default value to EEPROM now
+
+  // use the non noeeprom versions, to write these values to EEPROM too
+  rgblight_enable(); // Enable RGB by default
+  rgblight_sethsv(HSV_CYAN);  // Set it to CYAN by default
+  rgblight_mode(RGB_MATRIX_BREATHING); // set to solid by default
+  rgb_matrix_set_speed(LAYERS_EFFECT_SPEEDS[0]);
+}
---
+++ keyboards/bastardkb/charybdis/4x6/keymaps/dosssman/config.h
@@ -0,0 +1,118 @@
+/**
+ * Copyright 2021 Charly Delay <charly@codesink.dev> (@0xcharly)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#pragma once
+
+#define EE_HANDS
+
+/* RGB Matrix. */
+
+#ifdef RGB_MATRIX_ENABLE
+// Limit maximum brightness to keep power consumption reasonable, and avoid
+// disconnects.
+#    undef RGB_MATRIX_MAXIMUM_BRIGHTNESS
+#    define RGB_MATRIX_MAXIMUM_BRIGHTNESS 64
+
+// Rainbow swirl as startup mode.
+// Charybdis' default
+// #    define ENABLE_RGB_MATRIX_CYCLE_LEFT_RIGHT
+// #    define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_CYCLE_LEFT_RIGHT
+
+// Slow swirl at startup.
+#    define RGB_MATRIX_STARTUP_SPD 64 // Default: 32
+
+// Startup values.
+#    define RGB_MATRIX_STARTUP_HUE 0
+#    define RGB_MATRIX_STARTUP_SAT 255
+#    define RGB_MATRIX_STARTUP_VAL RGB_MATRIX_MAXIMUM_BRIGHTNESS
+#    define RGB_MATRIX_STARTUP_HSV RGB_MATRIX_STARTUP_HUE, RGB_MATRIX_STARTUP_SAT, RGB_MATRIX_STARTUP_VAL
+
+// Additional RGB profiles
+#    define ENABLE_RGB_MATRIX_BREATHING
+#    define ENABLE_RGB_MATRIX_BAND_VAL
+#    define ENABLE_RGB_MATRIX_BAND_SPIRAL_SAT
+// Typing related RGB matrix setings
+#    define ENABLE_RGB_MATRIX_SPLASH
+#    define ENABLE_RGB_MATRIX_SOLID_SPLASH
+
+// dosssman's override: startup default
+#    define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_BREATHING
+
+#endif  // RGB_MATRIX_ENABLE
+
+/* Trackball angle adjustment override. */
+#ifdef ROTATIONAL_TRANSFORM_ANGLE
+    #undef ROTATIONAL_TRANSFORM_ANGLE
+    #define ROTATIONAL_TRANSFORM_ANGLE -85
+#endif
+
+/** Invert X and Y axis on mouse reports overrides */
+// #define POINTING_DEVICE_INVERT_X
+// #define POINTING_DEVICE_INVERT_Y
+
+// Mouse config overrides
+#ifdef CHARYBDIS_MINIMUM_DEFAULT_DPI
+    #undef CHARYBDIS_MINIMUM_DEFAULT_DPI
+    #define CHARYBDIS_MINIMUM_DEFAULT_DPI 2100
+#endif
+
+#ifdef CHARYBDIS_DEFAULT_DPI_CONFIG_STEP
+    #undef CHARYBDIS_DEFAULT_DPI_CONFIG_STEP
+    #define CHARYBDIS_DEFAULT_DPI_CONFIG_STEP 300
+#endif
+
+#ifdef CHARYBDIS_MINIMUM_SNIPING_DPI
+    #define CHARYBDIS_MINIMUM_SNIPING_DPI 300
+#endif
+
+#ifdef CHARYBDIS_SNIPING_DPI_CONFIG_STEP
+    #undef CHARYBDIS_SNIPING_DPI_CONFIG_STEP
+    #define CHARYBDIS_SNIPING_DPI_CONFIG_STEP 100
+#endif
+
+// Charybdis custom keycodes extension for drag scroll and sniping modeS
+/*
+    I use QMK Configurator tool to design the layout. As of this writing, it does not
+    seem to support custom keycodes.
+    To use custom keycodes, we thus have to overwrite the keycodes in the JSON file
+    to use our custom keycode. As it is confusing to edit the "raw" JSON files, I use
+    F24, F23, etc... when configuring the layout on QMK config.
+    Once the JSON file is generated and saved, I can then look for the "F24" key,
+    then change it to a custom keycode, before genearting the "keymap.c" file, then
+    flashing to the keyboard.
+
+    The key codes are as follow:
+
+    #define DPI_MOD POINTER_DEFAULT_DPI_FORWARD
+    #define DPI_RMOD POINTER_DEFAULT_DPI_REVERSE
+    #define S_D_MOD POINTER_SNIPING_DPI_FORWARD
+    #define S_D_RMOD POINTER_SNIPING_DPI_REVERSE
+    #define SNIPING SNIPING_MODE
+    #define SNP_TOG SNIPING_MODE_TOGGLE
+    #define DRGSCRL DRAGSCROLL_MODE
+    #define DRG_TOG DRAGSCROLL_MODE_TOGGLE
+
+    As of this writing:
+    F24 -> DRGSCRL
+    F23 -> DPI_MOD
+    F22 -> S_D_MOD // SNIPING DPI FORWARD mode
+    F21 -> SNP_TOG
+
+    Note that for the DPI change, the "increase" command is set by default,
+    but it suffices to hold "Shift" key while pressing that key to send the
+    "decrease" command.
+
+*/
